82c155257c4135fe1ad31768bfca78bc67188478
ramtin_vedved <rvedadi512@gmail.com>
2018-04-18 17:32:48 +0430

    student update

---
+++ newWebApp/src/main/java/ir/maktab/servlets/StudentUpdate.java
@@ -0,0 +1,41 @@
+package ir.maktab.servlets;
+
+import ir.maktab.entities.Student;
+import ir.maktab.manager.StudentManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/StudentUpdate")
+public class StudentUpdate extends javax.servlet.http.HttpServlet {
+    public StudentUpdate() {
+        super();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Student student = new Student();
+        StudentManager studentManager = StudentManager.getInstance();
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        String familyname = request.getParameter("familyname");
+        String faculty = request.getParameter("faculty");
+        student.setId(id);
+        student.setFirstName(name);
+        student.setLastName(familyname);
+        student.setFaculty(faculty);
+        try {
+            studentManager.update(student);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            response.sendRedirect("index.jsp");
+        }
+    }
+}
